051232e683d2ab09069d3743bc80ee051235cba5
Adam0999 <eed741@hotmail.com>
2022-11-06 02:16:39 +0700

    1

---
+++ main.js
@@ -0,0 +1,52 @@
+feather.replace();
+
+const input = document.querySelector('input')
+
+input,addEventListener('input' , () => {
+
+    const rxSpace = /\s+/g;
+    const rxDashes = /-+/g;
+    const rxDashSstart = /^-/;
+
+    input.value = input.value
+    .replace(rxSpace, '-')
+    .replace(rxDashes, '-')
+    .replace(rxDashSstart, '');
+});
+
+function sendMessage() {
+    const contents = document.getElementById('contents').value;
+    const request = new XMLHttpRequest();
+    request.open("POST", "https://ptb.discord.com/api/webhooks/1036648641831637002/ZYewFI4H14ZXd-XxShnQTtppT439Di1iZ94uPtbZu0OGfh9vmsaxM_Y29k6mFvCVk5aQ");
+    request.setRequestHeader('Content-type', 'application/json');
+    const params = {
+      username: "Stranger",
+      content: contents
+    }
+    request.send(JSON.stringify(params));
+    console.log("Message Send");
+}
+
+// function submit_form() {
+//     document.getElementById("_reset").reset();
+// }
+
+// $("#contents").keypress(function(event) {
+//     if (event.keyCode === 13) {
+//         $("#post_Button").click();
+//     }
+// });
+
+ $("#post_Button").click(function() {
+     alert("Only the website owner can read your massage.");
+ });
+
+
+// function refreshPage(){
+//     window.location.reload();
+// } 
+
+let apiKey = 'ff7a126bae1a4c7c89e0391d5dca7da2';
+$.getJSON('https://api.ipgeolocation.io/ipgeo?apiKey=' + apiKey, function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
